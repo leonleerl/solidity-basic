cbfc314feb7bd7e99d4da130211ab8f0378866a3
Leon Lee <leon.runlongli@gmail.com>
2025-11-25 19:58:18 +0800

    day 4

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -13,6 +13,66 @@ contract VaultTest is Test {
         vault = new Vault();
     }
 
+    function testDeposit() public {
+        vm.deal(alice, 1 ether);
+        vm.prank(alice);
+        vault.deposit{value: 1 ether}();
+
+        assertEq(vault.balanceOf(alice), 1 ether);
+        assertEq(address(vault).balance, 1 ether);
+    }
+
+    function testWithdraw() public {
+        vm.deal(bob, 3 ether);
+
+        vm.startPrank(bob);
+        assertEq(vault.balanceOf(bob), 0);
+
+        vault.deposit{value: 2 ether}();
+        assertEq(vault.balanceOf(bob), 2 ether);
+
+        vault.withdraw(1 ether);
+        assertEq(vault.balanceOf(bob), 1 ether);
+        assertEq(address(vault).balance, 1 ether);
+        vm.stopPrank();
+    }
+
+    function testDeposit_MultiUser() public {
+        vm.deal(alice, 5 ether);
+        vm.deal(bob, 3 ether);
+
+        vm.startPrank(alice);
+        vault.deposit{value: 2 ether}();
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        vault.deposit{value: 1 ether}();
+        vm.stopPrank();
+
+        assertEq(address(vault).balance, 3 ether);
+        assertEq(vault.balanceOf(alice), 2 ether);
+        assertEq(vault.balanceOf(bob), 1 ether);
+
+        uint256 aliceBefore = alice.balance;
+        vm.startPrank(alice);
+        vault.withdraw(1 ether);
+        vm.stopPrank();
+
+        assertEq(alice.balance, aliceBefore + 1 ether);
+        assertEq(vault.balanceOf(alice), 1 ether);
+        assertEq(address(vault).balance, 2 ether);
+
+        uint256 bobBefore = bob.balance;
+
+        vm.startPrank(bob);
+        vault.withdraw(0.5 ether);
+        vm.stopPrank();
+
+        assertEq(vault.balanceOf(bob), 0.5 ether);
+        assertEq(address(vault).balance, 1.5 ether);
+        assertEq(bob.balance, bobBefore + 0.5 ether);
+    }
+
     function testOwnerWithdrawRevertIfNotOwner() public {
         // mimic a user
         vm.prank(alice);
